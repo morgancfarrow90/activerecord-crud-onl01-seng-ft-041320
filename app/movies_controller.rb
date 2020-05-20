56033bd8d2fe3f83884bfc935305b46d5e4f2294
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-20 13:24:46 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,8 +25,8 @@ end
 
 def can_be_created_in_a_block(:title =>"Home Alone", :release_date =>1990 )
   Movie.create do |m|
-    m.title = []
-    m.release_date = []
+    m.title = [:title]
+    m.release_date = [:release_date]
   end
 end
 
