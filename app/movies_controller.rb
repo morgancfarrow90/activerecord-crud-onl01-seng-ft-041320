0c200f499ba618efb82d4fe3350be8c30c7df1d5
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-20 13:33:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,8 +25,8 @@ end
 
 def can_be_created_in_a_block(hash)
   Movie.create do |key, value|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    key.title = "Home Alone"
+    key.release_date = 1990
   end
 end
 
