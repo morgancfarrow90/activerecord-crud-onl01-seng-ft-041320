498b00e9b6f75f85d5b1e61ee947ba3df9c76c3d
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-20 13:41:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args)
+def can_be_created_in_a_block( args = { title: "The Room", release_date: 2003 })
   args = {:title => "The Room", :release_date => ""
   Movie.create do |m, v|
     m.title = "Home Alone"
